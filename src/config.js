c253a82e747c14b37dac5a73ee85f2cd900504e0
System Administrator <root@Daniels-MacBook-Pro.local>
2020-07-02 14:23:08 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "serverlessnotess3"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://5csmtjgv45.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_d95dEsAFr",
+    APP_CLIENT_ID: "4nsgk4vjdtf87as2asl4f8c2pa",
+    IDENTITY_POOL_ID: "us-east-1:cdd3f011-19d9-4f79-b6ce-4e8181137c80"
+  }
+};
